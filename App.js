76e8587261e53511321812237fdd436b871d95ff
MrutyunjayaBehera <mrutyunjaya.9029@gmail.com>
2023-07-10 22:46:21 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Gossip</Text>
+    </View>
+  );
+}
+
+export default App;
